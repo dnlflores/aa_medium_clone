9b755e6dd6a0931362865be10c2df9140b94eb27
otmcz <chris_ramos1998@yahoo.com>
2021-11-15 16:36:15 -0500

    did user create

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const { User } = require('../db/models')
 
+const { asyncHandler, csrfProtection } = require('./utils')
 /* GET users listing. */
-router.get('/register', function(req, res, next) {
+
+router.get('/register', csrfProtection, asyncHandler(async function(req, res, next) {
+  const user = User.build()
   res.render('user-create',{
+    user,
     title: 'Register',
+    csrfToken: req.csrfToken()
   });
-});
+}));
 
 module.exports = router;
---
+++ routes/utils.js
@@ -0,0 +1,7 @@
+const bcrypt = require('bcrypt');
+const csrf = require('csurf');
+const csrfProtection= csrf({cookies: true});
+
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
+module.exports = {csrfProtection, asyncHandler}
