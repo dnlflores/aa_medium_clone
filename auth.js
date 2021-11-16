6ec25ed3b2f20a8be4abc4c4c142f326fa7ff789
baroobarecked <aaron@brubecksecure.com>
2021-11-16 08:32:30 -0600

    added a second if statement to check if auth had a userId

--- auth.js
+++ auth.js
@@ -6,6 +6,37 @@ const loginUser = (req, res, user) => {
   };
 };
 
+const restoreUser = async (req, res, next) => {
+  
+  console.log(req.session);
+
+  if (req.session.auth) {
+    const { userId } = req.session.auth;
+    if(userId) {
+      try {
+        const user = await User.findByPk(userId);
+  
+        if (user) {
+          res.locals.authenticated = true;
+          res.locals.user = user;
+          next();
+        }
+      } catch (err) {
+        res.locals.authenticated = false;
+        next(err);
+      }
+    } else {
+      res.locals.authenticated = false;
+      next();
+    }
+
+  } else {
+    res.locals.authenticated = false;
+    next();
+  }
+};
+
 module.exports = {
   loginUser,
+  restoreUser,
 };
