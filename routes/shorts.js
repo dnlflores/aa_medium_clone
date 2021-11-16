dcf7e5e81b4da886ba39d2ef93418716d9dc9dd0
Daniel Flores <daniel.flores4930@gmail.com>
2021-11-16 13:03:33 -0800

    implemented viewing shorts

--- routes/shorts.js
+++ routes/shorts.js
@@ -1,4 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+const { Short, User } = require('../db/models');
+const { check, validationResult } = require('express-validator');
+
+const { asyncHandler, csrfProtection, bcrypt } = require('./utils');
+
+router.get('/', asyncHandler(async (req, res, next) => {
+    const shorts = await Short.findAll({
+        order: [['updatedAt', 'DESC']],
+        limit: 15
+    });
+    res.render('shorts', {
+        title: "Shorts",
+        shorts
+    });
+}));
+
+router.get('/:id(\\d+)', asyncHandler(async (req, res, next) => {
+    const shortId = req.params.id;
+    const short = await Short.findByPk(shortId);
+    const user = await User.findByPk(short.userId);
+    res.render('short-page', {
+        title: short.title,
+        short,
+        username: user.username
+    });
+}));
+
 module.exports = router;
