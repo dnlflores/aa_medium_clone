bc6542e3eac9daee2407110236f7a0ece7fa60fb
Daniel Flores <daniel.flores4930@gmail.com>
2021-11-19 16:44:38 -0800

    fixed follow and other profile features

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,23 +1,27 @@
 window.addEventListener('DOMContentLoaded', () => {
   const feedButton = document.getElementById('feedButton');
   const myShortButton = document.getElementById('myShortButton');
-  if(myShortButton){
-    const userShortsList = document.getElementById('user-shorts-list');
-    const feedsShortsList = document.getElementById('feeds-shorts-list');
-    const userShortClassName = userShortsList.className;
-    const feedsShortClassName = feedsShortsList.className;
+  const userShortsList = document.getElementById('user-shorts-list');
+  const feedsShortsList = document.getElementById('feeds-shorts-list');
 
+  // console.log('VARIABLE TESTING\n', feedButton, myShortButton, userShortsList, feedsShortsList, userShortClassName, feedsShortClassName);
+  
+  if(myShortButton){
+    const userShortClassName = 'user-shorts-list-div feeds-shorts-list-div';
+    const feedsShortClassName = feedsShortsList.className;
+    
     feedButton.addEventListener("click", (event) => {
-      userShortsList.className="hidden"; //reset class name to hidden
-      feedsShortsList.className=feedsShortClassName;
-      feedButton.setAttribute("style", "color:#AABA49")
-      myShortButton.setAttribute("style", "color:#01523E")
+      userShortsList.className= "hidden"; //reset class name to hidden
+      feedsShortsList.className= feedsShortClassName;
+      feedButton.setAttribute("style", "color: #AABA49")
+      myShortButton.setAttribute("style", "color: #01523E")
     });
+
     myShortButton.addEventListener("click", (event) => {
-      feedsShortsList.className="hidden"; //reset class name to hidden
-      userShortsList.className=userShortClassName;
-      myShortButton.setAttribute("style", "color:#AABA49")
-      feedButton.setAttribute("style", "color:#01523E")
+      feedsShortsList.className=" hidden"; //reset class name to hidden
+      userShortsList.className= userShortClassName;
+      myShortButton.setAttribute("style", "color: #AABA49")
+      feedButton.setAttribute("style", "color: #01523E")
     });
   }
 })
